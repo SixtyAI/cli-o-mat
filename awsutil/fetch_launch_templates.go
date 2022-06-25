3999ea015e63293eb54cf92d0be8ad0a73d32d23
Jon Frisby <jfrisby@mrjoy.com>
2022-06-25 16:19:11 -0700

    Teach FetchLaunchTemplates how to look for a specific template.

--- awsutil/fetch_launch_templates.go
+++ awsutil/fetch_launch_templates.go
@@ -6,13 +6,19 @@ import (
 	"github.com/pkg/errors"
 )
 
-func FetchLaunchTemplates(ec2Client *ec2.EC2) ([]*ec2.LaunchTemplate, error) {
+func FetchLaunchTemplates(ec2Client *ec2.EC2, name *string) ([]*ec2.LaunchTemplate, error) {
 	nextToken := aws.String("")
 	templates := []*ec2.LaunchTemplate{}
 
 	for {
+		var names []*string
+		if name != nil {
+			names = []*string{name}
+		}
+
 		resp, err := ec2Client.DescribeLaunchTemplates(&ec2.DescribeLaunchTemplatesInput{
-			NextToken: nextToken,
+			LaunchTemplateNames: names,
+			NextToken:           nextToken,
 		})
 		if err != nil {
 			return nil, errors.Wrap(err, "failed to describe launch templates")
