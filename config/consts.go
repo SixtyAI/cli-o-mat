ece1ef5dbdaad0d0f33dc8f4cc80da57e71604f7
Jon Frisby <jfrisby@mrjoy.com>
2022-06-26 14:27:20 -0700

    gofmt.

--- config/consts.go
+++ config/consts.go
@@ -1,9 +1,11 @@
 package config
 
-const AppTag = "Application"
-const ASGTag = "aws:autoscaling:groupName"
-const CommitTag = "BuildCommit"
-const LTVersionTag = "aws:ec2launchtemplate:version"
-const NameTag = "Name"
-const ServiceTag = "Service"
-const TypeTag = "Type"
+const (
+	AppTag       = "Application"
+	ASGTag       = "aws:autoscaling:groupName"
+	CommitTag    = "BuildCommit"
+	LTVersionTag = "aws:ec2launchtemplate:version"
+	NameTag      = "Name"
+	ServiceTag   = "Service"
+	TypeTag      = "Type"
+)
