a50b6e2832a67714aabd427b18d2530ccbe5afd7
Jon Frisby <jfrisby@mrjoy.com>
2023-01-30 00:24:47 -0800

    Remove more lint.

--- awsutil/auth.go
+++ awsutil/auth.go
@@ -11,6 +11,12 @@ import (
 	"github.com/FasterBetter/cli-o-mat/config"
 )
 
+const (
+	CantFindRoleParam       = 10
+	ErrorLookingUpRoleParam = 11
+	RoleParamEmpty          = 12
+)
+
 func FindAndAssumeAdminRole(accountSlug string, omat *config.Omat) *config.SessionDetails {
 	paramPrefix := omat.Prefix()
 
@@ -24,17 +30,17 @@ func FindAndAssumeAdminRole(accountSlug string, omat *config.Omat) *config.Sessi
 	if err != nil {
 		if strings.HasPrefix(err.Error(), "ParameterNotFound") {
 			fmt.Printf("Couldn't find role parameter: %s\n", roleParamName)
-			os.Exit(10)
+			os.Exit(CantFindRoleParam)
 		}
 
 		fmt.Printf("Error looking up role parameter %s, got: %s\n", roleParamName, err)
-		os.Exit(11)
+		os.Exit(ErrorLookingUpRoleParam)
 	}
 
 	arn := aws.StringValue(roleParam.Parameter.Value)
 	if arn == "" {
 		fmt.Printf("Paramater '%s' was empty\n", roleParamName)
-		os.Exit(12)
+		os.Exit(RoleParamEmpty)
 	}
 
 	details := omat.Credentials.ForARN(arn)
